4dd401151c1baef0d04da618096952ce69fa5a0f
Alvyn Abranches <alvynabranches@gmail.com>
2022-10-19 01:42:42 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ def task_status(request: Request):
 def create_project(request: Request):
     try:
         data = request.json()
+        if "userID" not in data:
+            return JSONResponse({"status": "specify userID in json body"}, 400)
+        if "projectName" not in data:
+            return JSONResponse({"status": "specify projectName in json body"}, 400)
         return JSONResponse({"status": "ok"}, 200)
     except Exception:
         return JSONResponse({"status": "invalid json body"}, 400)
@@ -33,6 +37,18 @@ def create_project(request: Request):
 def create_user(request: Request):
     try:
         data = request.json()
+        return JSONResponse({"status": "ok"}, 200)
+    except Exception:
+        return JSONResponse({"status": "invalid json body"}, 400)
+
+@app.post("/train")
+def train(request: Request):
+    try:
+        data = request.json()
+        if "userID" not in data:
+            return JSONResponse({"status": "specify userID in json body"}, 400)
+        if "projectID" not in data:
+            return JSONResponse({"status": "specify projectID in json body"}, 400)
         return JSONResponse({"status": "ok"}, 200)
     except Exception:
         return JSONResponse({"status": "invalid json body"}, 400)
